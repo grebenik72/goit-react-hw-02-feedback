5fa69ba8933be95cc2b175e5e9406cbe1d41bbf1
grebenik72 <grebenik72@gmail.com>
2023-08-28 07:28:54 +0300

    finish2

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,21 +1,23 @@
-import { Button, Wrapper } from "./FeedbackOptions.styled";
-import PropTypes from 'prop-types'
+import { Button, Wrapper } from './FeedbackOptions.styled';
+import PropTypes from 'prop-types';
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => {
-    return (<Wrapper>
-        {options.map(option => (
-            <Button
-                type='button'
-                key={option}
-                onClick={() => onLeaveFeedback(option)}>
-                {option}
-            </Button>
-        ))}
+  return (
+    <Wrapper>
+      {options.map(option => (
+        <Button
+          type="button"
+          key={option}
+          onClick={() => onLeaveFeedback(option)}
+        >
+          {option}
+        </Button>
+      ))}
     </Wrapper>
-    );
+  );
 };
 
 FeedbackOptions.propTypes = {
-    options: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
-    onLeaveFeedback: PropTypes.func.isRequired,
-}
+  options: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
